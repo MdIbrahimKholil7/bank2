63a47e7f9fd8497ccfa5a7061c4af3ea10160f71
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-02-18 23:30:17 +0600

    code complete

--- bank2/index.js
+++ bank2/index.js
@@ -4,68 +4,62 @@ function getInput(inputField) {
     const input = document.getElementById(inputField);
     console.log(input)
     const newInput = parseFloat(input.value);
-    input.value='';
+    input.value = '';
     return newInput;
 }
 // get innerText 
-function getInnerText(input){
-    const innerTxt=document.getElementById(input);
-    const newInnerText=parseFloat(innerTxt.innerText);
+function getInnerText(input) {
+    const innerTxt = document.getElementById(input);
+    const newInnerText = parseFloat(innerTxt.innerText);
     return newInnerText;
 }
 // update deposite and withdraw balance 
-function getBalance(input,innerValue){
+function getBalance(input, innerValue) {
     // debugger;
     // const innerVal=document.getElementById(innerValue);
     // const newInnerVal=parseFloat(innerVal.innerText);
-    const total=(getInnerText(innerValue) + input);
-    return document.getElementById(innerValue).innerText=total;
+    const total = (getInnerText(innerValue) + input);
+    return document.getElementById(innerValue).innerText = total;
 }
 
 // update total balance 
-function updateBalance(input,isAdd){
+function updateBalance(input, isAdd) {
     // debugger
     console.log(getInnerText('deposite-blance'))
     // debugger
     // const total=document.getElementById('total-blance');
     // const newTotal=parseFloat(total.innerText);
-    
-    if(isAdd==false){
-        
-        const subTotal=getInnerText('deposite-blance') - getInput(input);
-        console.log(subTotal)
-       
-        const total=getInnerText('total-blance')+subTotal;
-        console.log(total)
-        return document.getElementById('total-blance').innerText=total
-    }else{
-        const fullTotal=(getInnerText('total-blance') + input);
-        return document.getElementById('total-blance').innerText=fullTotal
+    if (isAdd == false) {
+        const total = (getInnerText('total-blance') - input);
+        document.getElementById('total-blance').innerText = total
+    } else {
+        const fullTotal = (getInnerText('total-blance') + input);
+        document.getElementById('total-blance').innerText = fullTotal
     }
 }
 // adding deposite  
 document.getElementById('deposite-btn').addEventListener('click', function () {
     const input = getInput('deposite-input');
     // debugger
-    if(input < 0 || isNaN(input)){
-       return alert('Please input positive value');
-    }else{
-        const balance= getBalance(input,'deposite-blance');
+    if (input < 0 || isNaN(input)) {
+        return alert('Please input positive value');
+    } else {
+        const balance = getBalance(input, 'deposite-blance');
         updateBalance(input);
     }
 })
 // adding withdraw 
 document.getElementById('withdraw-btn').addEventListener('click', function () {
-//    debugger;
+    //    debugger;
     const input = getInput('withdraw-input');
-    console.log(input)
-    const balance= getBalance(input,'withdraw-blance');
-    if(isNaN(input) || (input < 0)){
-        alert('Please enter positive number')
-    }else if(((input > 0) && (input <= getInnerText('total-blance')))){
-        updateBalance(input,false);
-    }
-    
+    if (isNaN(input) || (input < 0)) {
+        return alert('Please enter positive number');
+     } else if (((input > 0) && (input <= getInnerText('total-blance')))) {
+         updateBalance(input, false);
+     }
+    const balance = getBalance(input, 'withdraw-blance');
+   
+
 })
 
 
